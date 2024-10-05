c69feec19b8e26a7cee966861ef9048f93be0567
nszandrew <viajantestemporais@gmail.com>
2024-10-05 13:29:54 -0300

    docs: Swagger update controller docs

--- src/main/java/com/example/demo/adapters/controller/MetasController.java
+++ src/main/java/com/example/demo/adapters/controller/MetasController.java
@@ -3,6 +3,10 @@ package com.example.demo.adapters.controller;
 import com.example.demo.adapters.service.MetasService;
 import com.example.demo.entities.projeto.dto.MetasDTO;
 import com.example.demo.entities.projeto.dto.MetasResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/metas")
+@Tag(name = "Metas")
 public class MetasController {
 
     private final MetasService metasService;
@@ -18,24 +23,49 @@ public class MetasController {
         this.metasService = metasService;
     }
 
+
+    @Operation(summary = "Cria uma nova meta para o projeto especificado pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Meta criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição (campos inválidos)"),
+            @ApiResponse(responseCode = "404", description = "Projeto não encontrado"),
+    })
     @PostMapping("/{id}")
     public ResponseEntity<MetasResponseDTO> createMetas(@RequestBody @Valid MetasDTO metasDTO, @PathVariable Long id) {
         var metas = metasService.createMeta(metasDTO, id);
         return new ResponseEntity<>(metas, HttpStatus.CREATED);
     }
 
+
+    @Operation(summary = "Recupera uma meta pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Meta recuperada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Meta não encontrada"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<MetasResponseDTO> getMetas(@PathVariable Long id) {
         var metas = metasService.getById(id);
         return new ResponseEntity<>(metas, HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Atualiza uma meta existente pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Meta atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição (campos inválidos)"),
+            @ApiResponse(responseCode = "404", description = "Meta ou projeto não encontrados"),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<MetasResponseDTO> updateMetas(@RequestBody @Valid MetasDTO metasDTO, @PathVariable Long id) {
         var metas = metasService.updateMeta(metasDTO, id);
         return new ResponseEntity<>(metas, HttpStatus.OK);
     }
 
+    @Operation(summary = "Exclui uma meta pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Meta excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Meta não encontrada"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity deleteMetas(@PathVariable Long id) {
         metasService.deleteMeta(id);
--- src/main/java/com/example/demo/adapters/controller/ProjetoController.java
+++ src/main/java/com/example/demo/adapters/controller/ProjetoController.java
@@ -3,6 +3,10 @@ package com.example.demo.adapters.controller;
 import com.example.demo.adapters.service.ProjetoService;
 import com.example.demo.entities.projeto.dto.ProjetoRequestDTO;
 import com.example.demo.entities.projeto.dto.ProjetoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projeto")
+@Tag(name = "Projetos")
 public class ProjetoController {
 
     private final ProjetoService service;
@@ -18,24 +23,45 @@ public class ProjetoController {
         this.service = service;
     }
 
+    @Operation(summary = "Cria um novo projeto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Projeto criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição (campos inválidos)"),
+    })
     @PostMapping
     public ResponseEntity<ProjetoResponseDTO> save(@RequestBody @Valid ProjetoRequestDTO data){
         var createProjeto = service.createProject(data);
         return new ResponseEntity<>(createProjeto, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Recupera um projeto pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Projeto recuperado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Projeto não encontrado"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ProjetoResponseDTO> getById(@PathVariable Long id){
         var projeto = service.getById(id);
         return ResponseEntity.ok(projeto);
     }
 
+    @Operation(summary = "Atualiza um projeto existente pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Projeto atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição (campos inválidos)"),
+            @ApiResponse(responseCode = "404", description = "Projeto não encontrado"),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<ProjetoResponseDTO> update(@PathVariable Long id, @RequestBody @Valid ProjetoRequestDTO data){
         var updateProject = service.updateProject(data, id);
         return ResponseEntity.ok(updateProject);
     }
 
+    @Operation(summary = "Exclui um projeto pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Projeto excluído com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Projeto não encontrado"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Long id){
         service.deleteProjectById(id);
